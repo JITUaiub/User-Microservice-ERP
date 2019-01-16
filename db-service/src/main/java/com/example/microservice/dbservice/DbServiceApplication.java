156dba940c47009eaecf48c410bd30b46b49a76b
Md. Nashid Kamal <nashidkamaljitu7@gmail.com>
2019-01-16 14:59:29 +0600

    Hystrix added for Service DB

--- db-service/src/main/java/com/example/microservice/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/example/microservice/dbservice/DbServiceApplication.java
@@ -2,12 +2,17 @@ package com.example.microservice.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
-@EnableJpaRepositories(basePackages = {"com.example.microservice.dbservice.repository"})
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableJpaRepositories(basePackages = {"com.example.microservice.dbservice.repository"})
+
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
--- db-service/src/main/java/com/example/microservice/dbservice/resources/DBResources.java
+++ db-service/src/main/java/com/example/microservice/dbservice/resources/DBResources.java
@@ -21,77 +21,91 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservice.dbservice.model.User;
 import com.example.microservice.dbservice.repository.UserRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/rest/db")
 public class DBResources {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@PersistenceContext
-	 EntityManager em;
-	
+	EntityManager em;
+
 	public DBResources() {
-	
+
 	}
-	
+
+	@HystrixCommand(fallbackMethod = "fallBackDB")
 	@GetMapping("/{username}")
-	public User getUser (@PathVariable("username") final String username){
-		
+	public User getUserFromDB(@PathVariable("username") final String username) {
+
 		CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<User> cq = cb.createQuery(User.class);
-        Root<User> rootEntry = cq.from(User.class);
-        CriteriaQuery<User> all = cq.select(rootEntry);
-        TypedQuery<User> allQuery = em.createQuery(all);
-        List<User> userList = allQuery.getResultList();
-		
-        for (User u : userList) {
+		CriteriaQuery<User> cq = cb.createQuery(User.class);
+		Root<User> rootEntry = cq.from(User.class);
+		CriteriaQuery<User> all = cq.select(rootEntry);
+		TypedQuery<User> allQuery = em.createQuery(all);
+		List<User> userList = allQuery.getResultList();
+
+		for (User u : userList) {
 			if (u.getUsername().equals(username)) {
 				return u;
 			}
 		}
-        return null;
+		return null;
 	}
-	
+
+	public User fallBackDB(@PathVariable("username") final String username) {
+
+		User defaultUser = new User();
+		defaultUser.setId(-1);
+		defaultUser.setName("Default User");
+		defaultUser.setUsername("Not Exist");
+		defaultUser.setPassword("Not Applicable");
+
+		return new User();
+	}
+
 	@PostMapping("/add")
-	public boolean addUser(@RequestBody final User user){
-		
+	public boolean addUser(@RequestBody final User user) {
+
 		userRepository.save(user);
 		return true;
 	}
-	
+
 	@DeleteMapping("/delete/{username}")
-	public void deleteUser(@PathVariable String username){
-		
+	public void deleteUser(@PathVariable String username) {
+
 		CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<User> cq = cb.createQuery(User.class);
-        Root<User> rootEntry = cq.from(User.class);
-        CriteriaQuery<User> all = cq.select(rootEntry);
-        TypedQuery<User> allQuery = em.createQuery(all);
-        List<User> userList = allQuery.getResultList();
+		CriteriaQuery<User> cq = cb.createQuery(User.class);
+		Root<User> rootEntry = cq.from(User.class);
+		CriteriaQuery<User> all = cq.select(rootEntry);
+		TypedQuery<User> allQuery = em.createQuery(all);
+		List<User> userList = allQuery.getResultList();
 		int delID = 0;
-        for (User u : userList) {
+		for (User u : userList) {
 			if (u.getUsername().equals(username)) {
 				delID = u.getId();
 				break;
 			}
 		}
-		
+
 		userRepository.deleteById(delID);
 	}
-	
+
 	@PutMapping("/update/{username}")
-	public void updateUser(@PathVariable String username, @RequestBody final User user){
-		
+	public void updateUser(@PathVariable String username,
+			@RequestBody final User user) {
+
 		CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<User> cq = cb.createQuery(User.class);
-        Root<User> rootEntry = cq.from(User.class);
-        CriteriaQuery<User> all = cq.select(rootEntry);
-        TypedQuery<User> allQuery = em.createQuery(all);
-        List<User> userList = allQuery.getResultList();
-		
-        for (User u : userList) {
+		CriteriaQuery<User> cq = cb.createQuery(User.class);
+		Root<User> rootEntry = cq.from(User.class);
+		CriteriaQuery<User> all = cq.select(rootEntry);
+		TypedQuery<User> allQuery = em.createQuery(all);
+		List<User> userList = allQuery.getResultList();
+
+		for (User u : userList) {
 			if (u.getUsername().equals(username)) {
 				u.setName(user.getName());
 				u.setPassword(user.getPassword());
